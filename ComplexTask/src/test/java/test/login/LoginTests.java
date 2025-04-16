05e6763ead53f23e2055bb1e03bf25d2d178c1a8
franfigueroa-17 <franciscofigueroa303@gmail.com>
2025-04-16 12:16:02 -0300

    Finished task

--- ComplexTask/src/test/java/test/login/LoginTests.java
+++ ComplexTask/src/test/java/test/login/LoginTests.java
@@ -8,12 +8,21 @@ import org.slf4j.Logger;
 import test.BaseTest;
 import utils.logger.LoggerSingleton;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class LoginTests extends BaseTest {
 
     private static final Logger log = LoggerSingleton.getLogger(LoginTests.class);
 
+    /**
+     * UC-1: Test Login with empty credentials
+     * Given I am on the login page
+     * When I type any credentials into username and password fields
+     * And I clear the inputs
+     * And I click on login
+     * Then I should see the error message: "Username is required"
+     */
     @ParameterizedTest
     @MethodSource("utils.data.DataProviders#invalidCredentials")
     public void LoginWithEmptyCredentials(String username, String password) {
@@ -28,12 +37,17 @@ public class LoginTests extends BaseTest {
         String result = loginPage.getResult();
         log.info("Received message: " + result);
 
-        if (!result.equals("Epic sadface: Username is required")) {
-            log.error("Wrong Result. Expected: 'Epic sadface: Username is required', but obtained: '" + result + "'");
-        }
-        assertEquals("Epic sadface: Username is required", result);
+        assertThat("Wrong Result", result, is("Epic sadface: Username is required"));
     }
 
+    /**
+     * UC-2: Test Login with only username
+     * Given I am on the login page
+     * When I enter a valid username
+     * And I clear the password input
+     * And I click on login
+     * Then I should see the error message: "Password is required"
+     */
     @ParameterizedTest
     @MethodSource("utils.data.DataProviders#validCredentials")
     public void LoginWithUsernameOnly(String username, String password) {
@@ -47,12 +61,16 @@ public class LoginTests extends BaseTest {
         String result = loginPage.getResult();
         log.info("Received message: " + result);
 
-        if (!result.equals("Epic sadface: Password is required")) {
-            log.error("Wrong Result. Expected: 'Epic sadface: Password is required', but obtained: '" + result + "'");
-        }
-        assertEquals("Epic sadface: Password is required", result);
+        assertThat("Wrong Result", result, is("Epic sadface: Password is required"));
     }
 
+    /**
+     * UC-3: Test Login with valid credentials
+     * Given I am on the login page
+     * When I enter a valid username and password
+     * And I click on login
+     * Then I should be redirected to homepage and see the title "Swag Labs"
+     */
     @ParameterizedTest
     @MethodSource("utils.data.DataProviders#validCredentials")
     public void LoginWithUserAndPassword(String username, String password) {
@@ -63,10 +81,11 @@ public class LoginTests extends BaseTest {
             HomePage homepage = loginPage.login(username, password);
             String logo = homepage.getLogoHomePage();
             log.info("Homepage Info: " + logo);
-            assertEquals("Swag Labs", logo);
+
+            assertThat("Wrong page title after login", logo, is("Swag Labs"));
         } catch (NoSuchElementException e) {
             log.error("Logo Element not found in the homepage");
-            fail("Login failed: credentials did not allow to log in into the homepage");
+            throw new AssertionError("Login failed: credentials did not allow to log in into the homepage", e);
         }
     }
 }
